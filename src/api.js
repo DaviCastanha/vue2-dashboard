1b6e563789108640ce2a10cfe0b8da5b1ecdae8d
Davi Castanha <davicastanha.esc@gmail.com>
2025-09-23 20:02:39 -0300

    api log

--- src/api.js
+++ src/api.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 const baseURL = process.env.VUE_APP_API_URL || 'http://localhost:3000';
-
-const api = axios.create({
-    baseURL,
-});
+console.log('API URL usada:', baseURL);
+const api = axios.create({baseURL});
 
 export default api
