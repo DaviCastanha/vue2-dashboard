9f3797f2192ad1c2e01e29d67adc9dc8a41981a2
Davi Castanha <davicastanha.esc@gmail.com>
2025-09-23 20:46:53 -0300

    Cors

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+const cors = require('cors');
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('server/db.json');
 const middlewares = jsonServer.defaults();
 
 const PORT = process.env.PORT || 3000;
-
+server.use(cors());
 server.use(middlewares);
 server.use(router);
 server.listen(PORT, () => {
